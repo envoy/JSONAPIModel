3b94f658ab267669da5ce5bfd1da8529e97ce361
Fang-Pen Lin <hello@fangpenlin.com>
2018-09-25 13:09:41 -0700

    Init project

---
+++ JSONAPIModel/JSONAPIModel.h
@@ -0,0 +1,19 @@
+//
+//  JSONAPIModel.h
+//  JSONAPIModel
+//
+//  Created by Fang-Pen Lin on 9/25/18.
+//  Copyright © 2018 Envoy Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONAPIModel.
+FOUNDATION_EXPORT double JSONAPIModelVersionNumber;
+
+//! Project version string for JSONAPIModel.
+FOUNDATION_EXPORT const unsigned char JSONAPIModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONAPIModel/PublicHeader.h>
+
+
