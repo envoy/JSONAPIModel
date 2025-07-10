f33ad73737d6ed256cd7a0c1e2a8c4f540a9687b
Matthew Shehan <mattshehan@envoy.com>
2025-07-10 15:49:25 -0700

    add tests as part of package + platform

--- Package.swift
+++ Package.swift
@@ -1,22 +1,29 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "JSONAPIModel",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
         .library(
             name: "JSONAPIModel", 
             targets: ["JSONAPIModel"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git",
-                 from: "5.0.0"),
+      .package(
+        url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
     ],
     targets: [
         .target(
             name: "JSONAPIModel",
             dependencies: ["SwiftyJSON"],
             path: "JSONAPIModel"),
+        .testTarget(
+          name: "JSONAPIModelTests",
+          dependencies: ["JSONAPIModel"],
+          path: "JSONAPIModelTests")
     ]
 )
