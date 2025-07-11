2db7ac162e474621363789496103c1126938ceec
Matthew Shehan <mattshehan@envoy.com>
2025-07-10 18:28:24 -0700

    update tests to check iterative solution

--- JSONAPIModelTests/JSONAPIModelTests.swift
+++ JSONAPIModelTests/JSONAPIModelTests.swift
@@ -20,10 +20,11 @@ class JSONAPIModelTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-    
+
     func readJSON(fileName: String) -> JSON {
-        let bundle = Bundle(for: type(of: self))
-        let path = bundle.path(forResource: fileName, ofType: "json")!
+        let test = Bundle.module.bundlePath
+        print(test)
+        let path = Bundle.module.path(forResource: fileName, ofType: "json")!
         let data = try! Data(contentsOf: URL(fileURLWithPath: path))
         return JSON(try! JSONSerialization.jsonObject(
             with: data,
@@ -44,7 +45,7 @@ class JSONAPIModelTests: XCTestCase {
             XCTAssertEqual(model.id, "class-room-001")
             XCTAssertEqual(model.name, "Class Room 202")
             XCTAssertEqual(model.students.count, 3)
-            
+
             let store = JSONAPIStore(includedRecords: json["included"])
             try jsonModel?.loadIncluded(factory, store: store)
 
@@ -60,12 +61,61 @@ class JSONAPIModelTests: XCTestCase {
             XCTAssertEqual(secondBook.id, "book-002")
             XCTAssertEqual(secondBook.name, "Head First Programming")
             XCTAssertEqual(secondBook.isbn, "978-0596802370")
-            
+
             let marry = model.students[1]
             XCTAssertEqual(marry.id, "student-002")
             XCTAssertEqual(marry.name, "Marry")
             XCTAssertEqual(marry.books.count, 0)
-            
+
+            let jenny = model.students[2]
+            XCTAssertEqual(jenny.id, "student-003")
+            XCTAssertEqual(jenny.name, "Jenny")
+            XCTAssertEqual(jenny.books.count, 1)
+            let thirdBook = jenny.books[0]
+            XCTAssertEqual(thirdBook.id, "book-003")
+            XCTAssertEqual(thirdBook.name, "Effective Modern C++")
+            XCTAssertEqual(thirdBook.isbn, "978-1491903995")
+        } catch {
+            XCTFail()
+        }
+    }
+
+
+    func testLoadJSONAPIPayloadUsingIterative() {
+        let factory = JSONAPIFactory.defaultFactory
+        let json = readJSON(fileName: "payload")
+        do {
+            let jsonModel = try factory.createModel(json["data"])
+            guard let model = jsonModel as? ClassRoom else {
+                XCTFail()
+                return
+            }
+            XCTAssert(type(of: model) === ClassRoom.self)
+            XCTAssertEqual(model.id, "class-room-001")
+            XCTAssertEqual(model.name, "Class Room 202")
+            XCTAssertEqual(model.students.count, 3)
+
+            let store = JSONAPIStore(includedRecords: json["included"])
+            try jsonModel?.loadIncludedIterative(factory, store: store)
+
+            let john = model.students[0]
+            XCTAssertEqual(john.id, "student-001")
+            XCTAssertEqual(john.name, "John")
+            XCTAssertEqual(john.books.count, 2)
+            let firstBook = john.books[0]
+            XCTAssertEqual(firstBook.id, "book-001")
+            XCTAssertEqual(firstBook.name, "Head First Python")
+            XCTAssertEqual(firstBook.isbn, "978-1449382674")
+            let secondBook = john.books[1]
+            XCTAssertEqual(secondBook.id, "book-002")
+            XCTAssertEqual(secondBook.name, "Head First Programming")
+            XCTAssertEqual(secondBook.isbn, "978-0596802370")
+
+            let marry = model.students[1]
+            XCTAssertEqual(marry.id, "student-002")
+            XCTAssertEqual(marry.name, "Marry")
+            XCTAssertEqual(marry.books.count, 0)
+
             let jenny = model.students[2]
             XCTAssertEqual(jenny.id, "student-003")
             XCTAssertEqual(jenny.name, "Jenny")
