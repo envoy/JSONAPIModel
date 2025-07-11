0dae5d469dcba5b040fbcf3efb956cb305f5f405
Matthew Shehan <mattshehan@envoy.com>
2025-07-10 18:25:30 -0700

    update package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,17 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "JSONAPIModel",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12),
+        .macOS(.v10_14),
+        .tvOS(.v12),
     ],
     products: [
         .library(
-            name: "JSONAPIModel", 
+            name: "JSONAPIModel",
             targets: ["JSONAPIModel"]),
     ],
     dependencies: [
@@ -20,10 +22,15 @@ let package = Package(
         .target(
             name: "JSONAPIModel",
             dependencies: ["SwiftyJSON"],
-            path: "JSONAPIModel"),
+            path: "JSONAPIModel",
+            exclude: ["Info.plist"]),
         .testTarget(
           name: "JSONAPIModelTests",
           dependencies: ["JSONAPIModel"],
-          path: "JSONAPIModelTests")
+          path: "JSONAPIModelTests",
+          resources: [
+            .copy("Fixtures/payload.json"),
+            .copy("Fixtures/device-config.json"),
+          ])
     ]
 )
